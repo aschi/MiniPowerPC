e7f91edae4045346c08ac0649bd38dd440b82f69
Adrian Schmid <schmiad1@students.zhaw.ch>
2011-11-03 11:37:43 +0100

    "Test" main klasse bereinigt

--- src/main/java/ch/hszt/MiniPowerPC/Test.java
+++ src/main/java/ch/hszt/MiniPowerPC/Test.java
@@ -11,12 +11,12 @@ public class Test {
 		
 		MemoryEntry[] m = new MemoryEntry[1023];
 		
-		m[100] = new MemoryEntry("0100000111110100".toCharArray());
-		System.out.println(String.valueOf(m[100].getBinaryString()));
-		m[102] = new MemoryEntry("0100010111110110".toCharArray());
-		m[104] = new MemoryEntry("0000011110000000".toCharArray());
-		m[106] = new MemoryEntry("0110000111111000".toCharArray());
- 		m[500] = MemoryEntry.parseMemoryEntry(100);
+		m[100] = new MemoryEntry("0100000111110100".toCharArray()); //LWDD 0, 500
+		m[102] = new MemoryEntry("0100010111110110".toCharArray()); //LWDD 1, 502
+		m[104] = new MemoryEntry("0000011110000000".toCharArray()); //ADD 1
+		m[106] = new MemoryEntry("0110000111111000".toCharArray()); //SWDD 0, 504
+ 		
+		m[500] = MemoryEntry.parseMemoryEntry(100);
 		m[502] = MemoryEntry.parseMemoryEntry(150);
 		
 		MiniPowerPC emu = new MiniPowerPC(m);
